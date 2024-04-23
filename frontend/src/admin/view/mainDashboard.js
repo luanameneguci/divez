847edead7f014e794ba19058e5195a2d7e468237
Andre-Pascoal <aspsystem2001@gmail.com>
2024-04-23 09:58:23 +0100

    Criacao frontend

---
+++ frontend/src/admin/view/mainDashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+export default function ListComponent() {
+    return (
+        //
+    );
+}
