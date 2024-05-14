5897872ac2b43bd40520d1efd2b41aae9396b310
iuripires99 <133361855+iuripires99@users.noreply.github.com>
2024-05-14 09:48:52 +0100

    dasda
    asdasdasd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-import Menu from './admin/view/menu';
-import AdminDashboard from './admin/view/adminDashboard';
+import Menu from './views/admin/adminMenu';
+import AdminDashboard from './views/admin/adminDashboard';
+import BuyerMenu from './views/buyer/buyerMenu';
+import BuyerDashboard from './views/buyer/buyerDashboard';
+import ManagerMenu from './views/manager/managerMenu';
+import ManagerDashboard from './views/manager/managerDashboard';
+
 function App() {
   return (
     <div>
-      <Menu />
+      <BuyerMenu />
     </div>
 
   );
