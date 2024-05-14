2e612d32af2d32f5078f7c478dc6c64d308eb1ec
Andre-Pascoal <aspsystem2001@gmail.com>
2024-05-14 10:44:41 +0100

    Com componentes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Menu from './views/admin/adminMenu';
-import AdminDashboard from './views/admin/adminDashboard';
+
 import BuyerMenu from './views/buyer/buyerMenu';
 import BuyerDashboard from './views/buyer/buyerDashboard';
 import ManagerMenu from './views/manager/managerMenu';
@@ -9,7 +9,7 @@ import ManagerDashboard from './views/manager/managerDashboard';
 function App() {
   return (
     <div>
-      <BuyerMenu />
+      <Menu />
     </div>
 
   );
--- frontend/src/views/admin/adminDashboard.js
+++ frontend/src/views/admin/adminDashboard.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import '../../App.css';
+import notificationicon from "../../images/notification.png";
 
 const AdminDashboard = () => {
     return (
-        <p className="vinte">200000000000000000000000000000000000</p>
+        <div className="dashboard-content h-100 bg-light w-100">
+            <h4 className="title">Dashboard</h4>
+            <div class="container text-center">
+                <div class="row">
+                    <div class="col">
+                        <Box title="Pending tickets" number="20" image={notificationicon} evolution="10" />
+                    </div>
+                    <div class="col">
+                        <Box title="Pending tickets" number="20" image={notificationicon} evolution="10" />
+                    </div>
+                    <div class="col">
+                        <Box title="Pending tickets" number="20" image={notificationicon} evolution="10" />
+                    </div>
+                    <div class="col">
+                        <Box title="Pending tickets" number="20" image={notificationicon} evolution="10" />
+                    </div>
+                </div>
+            </div>
+        </div>
+
     );
+}
+
+function Box(props) {
+    return <div className="box-container bg-white col-auto rounded">
+        <span className="box-title">{props.title}</span>
+        <span className="box-number">{props.number}</span>
+        <img src={props.image} alt="" className="box-image" />
+        <span className="box-evolution">{props.evolution}% </span>
+    </div>;
 }
 export default AdminDashboard;
