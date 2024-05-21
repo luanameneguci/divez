b957dca98e0572050b4295ed7d8841e367c266b2
iuripires99 <133361855+iuripires99@users.noreply.github.com>
2024-05-21 08:49:02 +0100

    aasd
    asd

--- frontend/src/views/buyer/buyerDashboard.js
+++ frontend/src/views/buyer/buyerDashboard.js
@@ -9,13 +9,13 @@ const AdminDashboard = () => {
             <div class="container text-center">
                 <div class="row">
                     <div class="col">
-                        <Box title="Pending tickets" number="20" image={notificationicon} evolution="10" />
+                        <Box title="Pending budgets" number="20" image={notificationicon} evolution="10" />
                     </div>
                     <div class="col">
-                        <Box title="Pending tickets" number="20" image={notificationicon} evolution="10" />
+                        <Box title="Active Licenses" number="2000" image={notificationicon} evolution="10" />
                     </div>
                     <div class="col">
-                        <Box title="Pending tickets" number="20" image={notificationicon} evolution="10" />
+                        <Box title="Linked Users" number="200" image={notificationicon} evolution="10" />
                     </div>
                 </div>
             </div>
@@ -24,11 +24,15 @@ const AdminDashboard = () => {
 };
 
 function Box(props) {
-    return <div className="box-container bg-white col-auto rounded">
-        <span className="box-title d-block">{props.title}</span>
-        <span className="box-number d-block">{props.number}</span>
+    return <div className="box-container bg-white col-auto rounded d-flex px-4 py-4">
+        <div className="col-10">
+        <span className="box-title d-flex justify-content-start"><strong><h6>{props.title}</h6></strong></span>
+        <span className="box-number d-flex justify-content-start pt-2"><strong><h2>{props.number}</h2></strong></span>
+        <span className="box-evolution d-flex justify-content-bottom pt-2">{props.evolution}% more than yesterday </span>
+        </div>
+        <div>
         <img src={props.image} alt="" className="box-image ms-3" />
-        <span className="box-evolution d-block">{props.evolution}% </span>
+        </div>
     </div>;
 }
 export default AdminDashboard;
