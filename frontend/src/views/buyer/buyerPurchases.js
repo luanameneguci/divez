06c5276e7bf8fb4f0cef13575d8f5532a44206b1
iuripires99 <133361855+iuripires99@users.noreply.github.com>
2024-06-04 15:54:53 +0100

    asdasd

--- frontend/src/views/buyer/buyerPurchases.js
+++ frontend/src/views/buyer/buyerPurchases.js
@@ -34,38 +34,36 @@ const BuyerPurchasesList = () => {
 }
 
 function Products() {
-    return <div className="box-container col-auto d-flex shadow">
-        <div className="col-12 bg-white">
-            <table className='container-fluid py-4'>
-                <thead className='text-black text-center bg-info'>
-                    <th className="py-2">ID</th>
-                    <th className="py-2">Product</th>
-                    <th className="py-2">Date</th>
-                    <th className="py-2">Amount</th>
-                    <th className="py-2">Price</th>
-                </thead>
-                <tbody className='text-center rounded'>
-                    {rows.map((row, rowIndex) => (
-                        <tr key={rowIndex}>
-                            {row.map((data, colIndex) => (
-                                <td
-                                    key={colIndex}
-                                    className='border-bottom p-3'
-                                    style={{
-                                        color: colIndex === 6 ? '#FFD56D' : 'inherit',
-                                        padding: '15px 0 15px 0%',
-                                    }}
-                                >
-                                    {data}
-                                </td>
-
-                            ))}
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+    return <div className="box-container bg-white col-auto rounded d-flex shadow">
+    <div className="col-12 bg-info rounded">
+        <table className='container-fluid text-start bg-info py-4 rounded table3'>
+            <thead className='text-white'>
+                <th className="ps-3 py-2">ID</th>
+                <th className="ps-3 py-2">Product</th>
+                <th className="ps-3 py-2">Date</th>
+                <th className="ps-3 py-2">Amount</th>
+                <th className="ps-3 py-2">Price</th>
+            </thead>
+            <tbody className='bg-white'>
+            {rows.map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                        {row.map((data, colIndex) => (
+                           <td
+                           key={colIndex}
+                           style={{ 
+                               color: colIndex === 5 ? '#FFD56D' : 'inherit',
+                               padding: '10px 0 10px 1%' 
+                           }}
+                       >
+                            {data}
+                          </td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
     </div>
+</div>
 }
 
 
