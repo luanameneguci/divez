4f11a780ef52ce7b8f795960e7f3acf9031b806d
Andre-Pascoal <aspsystem2001@gmail.com>
2024-06-22 20:19:23 +0100

    Comite

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,7 +7,7 @@ import BuyerMenu from './views/buyer/buyerMenu';
 function App() {
   return (
     <div>
-      <AdminMenu />
+      <BuyerMenu />
     </div>
 
   );
--- frontend/src/views/buyer/buyerProductItem.js
+++ frontend/src/views/buyer/buyerProductItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState , useEffect} from "react";
 import "../../App.css";
 import Rating from "@mui/material/Rating";
 import ManagersList from "../../components/buyer/ManagersList";
